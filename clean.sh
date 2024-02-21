e6103f9e471ebbaa8ebde76e815a700548ffbcb4
Tim Sutton <tim@kartoza.com>
2024-02-21 22:26:35 +0000

    Added vscode setup

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+rm -rf .venv
+rm -rf .vscode-extensions
+rm -rf __pycache__
+cd ..
+cd -
+
--- shell.nix
+++ shell.nix
@@ -18,6 +18,7 @@ in pkgs.mkShell rec {
     pythonPackages.paramiko
     pythonPackages.pygobject3
     
+    pkgs.vscode
 
     # Those are dependencies that we would like to use from nixpkgs, which will
     # add them to PYTHONPATH and thus make them accessible from within the venv.
@@ -25,8 +26,39 @@ in pkgs.mkShell rec {
 
   # Run this command, only after creating the virtual environment
   postVenvCreation = ''
-    unset SOURCE_DATE_EPOCH
-    pip install -r requirements.txt
+     export DIRENV_LOG_FORMAT=
+     unset SOURCE_DATE_EPOCH
+     pip install -r requirements.txt
+  '';
+
+  shellHook = ''
+     echo "-----------------------"
+     echo "🌈 Your Hetzner Dev Environment is prepared."
+     echo "Run ./menu.py to start the gui"
+     echo ""
+     echo "🪛 Installing VSCode Extensions:"
+     echo "--------------------------------"
+     code --extensions-dir=".vscode-extensions" --install-extension donjayamanne.python-environment-manager
+     code --extensions-dir=".vscode-extensions" --install-extension donjayamanne.python-extension-pack
+     code --extensions-dir=".vscode-extensions" --install-extension hbenl.vscode-test-explorer
+     code --extensions-dir=".vscode-extensions" --install-extension jamesqquick.python-class-generator
+     code --extensions-dir=".vscode-extensions" --install-extension KevinRose.vsc-python-indent
+     code --extensions-dir=".vscode-extensions" --install-extension littlefoxteam.vscode-python-test-adapter
+     code --extensions-dir=".vscode-extensions" --install-extension maziac.asm-code-lens
+     code --extensions-dir=".vscode-extensions" --install-extension ms-python.debugpy
+     code --extensions-dir=".vscode-extensions" --install-extension ms-python.python
+     code --extensions-dir=".vscode-extensions" --install-extension ms-python.vscode-pylance
+     code --extensions-dir=".vscode-extensions" --install-extension ms-vscode.test-adapter-converter
+     code --extensions-dir=".vscode-extensions" --install-extension njpwerner.autodocstring
+     code --extensions-dir=".vscode-extensions" --install-extension VisualStudioExptTeam.intellicode-api-usage-examples
+     code --extensions-dir=".vscode-extensions" --install-extension VisualStudioExptTeam.vscodeintellicode
+     echo ""
+     echo "📒 Note:"
+     echo "-----------------------"
+     echo "start vscode like this:"
+     echo ""
+     echo "code --extensions-dir=\".vscode-extensions\" ."
+     echo "-----------------------"
   '';
 
   # Now we can execute any commands within the virtual environment.
